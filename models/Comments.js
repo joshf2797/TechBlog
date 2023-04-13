bda78ffd52b62c3347ddc6048d96d01b1c59fe01
Joshua <joshf2797@gmail.com>
2023-04-12 19:31:22 -0700

    added code from mini project, started working on updating models and seeds and whatnots

---
+++ models/Comments.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+// set up fields and rules for Comment model
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,22 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comments = require('./Comments')
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comments, {
+  foreignKey: 'post_id',
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { Post, Comments, User };
